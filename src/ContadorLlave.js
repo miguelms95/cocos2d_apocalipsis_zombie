c4a046bd5487710e993dbeb3dbb6587b2c4d9381
Miguel Martínez Serrano <miguelms.ovi@gmail.com>
2017-12-23 12:23:42 +0100

    fix tamaño llave contador

--- src/ContadorLlave.js
+++ src/ContadorLlave.js
@@ -21,6 +21,8 @@ var ContadorLlave = cc.Class.extend({
         this.layer.removeChild(this.sprite);
         this.sprite = new cc.Sprite(res.llave_activada_png);
         this.sprite.setPosition(this.posicionX, 420);
+        this.sprite.setScaleX(0.5);
+        this.sprite.setScaleY(0.5);
         this.activada = 1;
         this.layer.addChild(this.sprite);
     }
