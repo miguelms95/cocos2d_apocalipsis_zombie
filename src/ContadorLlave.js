657efa6a6266ba81fd98f2ddaf30dad510d901f5
DanielAlba96 <danielalba1996@hotmail.es>
2017-12-23 10:40:59 +0100

    kjl

--- src/ContadorLlave.js
+++ src/ContadorLlave.js
@@ -1,9 +1,8 @@
-
-var ContadorLLave = cc.Class.extend({
-    gameLayer:null,
-    sprite:null,
-    shape:null,
-    ctor:function (posicionX) {
+var ContadorLlave = cc.Class.extend({
+    gameLayer: null,
+    sprite: null,
+    shape: null,
+    ctor: function(posicionX) {
         this.sprite = new cc.Sprite(res.llave_gris_png);
 
         // this.shape.setSensor(true); // Nunca genera colisiones reales, es como un “fantasma”
@@ -13,7 +12,8 @@ var ContadorLLave = cc.Class.extend({
         this.sprite.setScaleX(0.5);
         this.sprite.setScaleY(0.5);
 
-    }, activar: function (){
+    },
+    activar: function() {
         this.sprite = new cc.Sprite(res.llave_activada_png);
     }
 });
